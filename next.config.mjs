4aff56c7076415e384bf4e20a2d51e7704b4be25
Markus Williams <60374806+MarkusWilliamsDev@users.noreply.github.com>
2024-08-30 21:25:48 -0400

    Playground video scroll (#14)
    * Add video scroll playground
    
    * Fix link covered by nav
    
    * Rename example playground
    
    * Fix margin on code playground header
    
    * cache earth-compressed.mp4
    
    * Use compressed earth scroll video
    
    * Update challenges faced
    
    * Remove old earth video

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	async headers() {
+		return [
+			{
+				source: "/earth-compressed.mp4",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=31536000, immutable",
+					},
+					{
+						key: "Accept-Ranges",
+						value: "bytes",
+					},
+				],
+			},
+		];
+	},
+};
 
 export default nextConfig;
