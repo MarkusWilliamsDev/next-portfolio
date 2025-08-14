68c00caee18b7909a996477f0024d25a02ffe5db
MarkusWilliams <markuswilliamsdev@gmail.com>
2025-08-14 11:56:54 -0400

    Fix hero cutoff on short screens

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
 	let keyWordCharArray = keyWord.split("");
 
 	return (
-		<div className="relative bg-gray-50/75 overflow-hidden h-screen">
+		<div className="relative bg-gray-50/75 overflow-hidden min-h-screen">
 			<ParticleBackground />
 			<div className="relative pt-16 pb-16 sm:pb-24 z-10">
 				<main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
