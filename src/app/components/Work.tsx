7c54adbed1176db281fd665499ac2e7e5d11bb74
MarkusWilliams <markuswilliamsdev@gmail.com>
2025-08-14 12:43:01 -0400

    Make buttons be consistent height in work cards

--- src/app/components/Work.tsx
+++ src/app/components/Work.tsx
@@ -75,11 +75,14 @@ export default function Work() {
   return (
     <div id="work" className="relative bg-gray-50">
       <div className="mx-auto pb-24 px-4 max-w-7xl sm:px-6 lg:px-8">
-        <h1 className="text-indigo-900 text-center  font-semibold text-4xl mb-8">My Work</h1>
+        <h1 className="text-indigo-900 text-center font-semibold text-4xl mb-8">My Work</h1>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           {allProjects.map((project, index) => (
-            <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
-              <div className="flex flex-col sm:flex-row">
+            <div
+              key={index}
+              className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col h-full"
+            >
+              <div className="flex flex-col sm:flex-row flex-1">
                 <div className="flex-shrink-0 p-6 pr-0 flex justify-center sm:justify-start w-full sm:w-1/3">
                   <Image
                     className="object-contain h-32 w-full"
@@ -87,41 +90,43 @@ export default function Work() {
                     alt={`${project.name} logo`}
                   />
                 </div>
-                <div className="flex-1 p-6 pt-0 sm:pt-6 w-full sm:w-2/3">
-                  <p className="text-gray-500 mb-4">{project.about}</p>
-                  {project.techUsed && (
-                    <div className="flex items-center text-gray-600 mb-4">
-                      <p className="pr-2 text-sm">Powered by</p>
-                      {project.techUsed.map((TechLogo, techIndex) => (
-                        <div key={techIndex} className="w-5 mx-1">
-                          <TechLogo className="w-full h-full hover:text-purple-800 duration-300" />
-                        </div>
-                      ))}
+                <div className="flex-1 p-6 pt-0 sm:pt-6 w-full sm:w-2/3 flex flex-col">
+                  <p className="text-gray-500 mb-4 flex-1">{project.about}</p>
+                  <div className="mt-auto">
+                    {project.techUsed && (
+                      <div className="flex items-center text-gray-600 mb-4">
+                        <p className="pr-2 text-sm">Powered by</p>
+                        {project.techUsed.map((TechLogo, techIndex) => (
+                          <div key={techIndex} className="w-5 mx-1">
+                            <TechLogo className="w-full h-full hover:text-purple-800 duration-300" />
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                    <div className="flex space-x-2">
+                      {project.github && (
+                        <a
+                          href={project.github}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="flex items-center justify-center bg-gray-200 space-x-2 rounded-lg shadow-sm p-2 text-center hover:outline outline-purple-800 text-slate-600 hover:text-purple-800 fill-slate-600 hover:fill-purple-800 hover:bg-gray-300 hover:shadow-md transition-all duration-200"
+                        >
+                          <Github />
+                          <p className="text-sm">View Code</p>
+                        </a>
+                      )}
+                      {project.link && (
+                        <a
+                          href={project.link}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="flex items-center justify-center bg-gray-200 space-x-2 rounded-lg shadow-sm p-2 text-center hover:outline outline-purple-800 text-slate-600 hover:text-purple-800 fill-slate-600 hover:fill-purple-800 hover:bg-gray-300 hover:shadow-md transition-all duration-200"
+                        >
+                          <p className="text-sm">Visit Site</p>
+                          <LinkIcon />
+                        </a>
+                      )}
                     </div>
-                  )}
-                  <div className="flex space-x-2">
-                    {project.github && (
-                      <a
-                        href={project.github}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="flex items-center justify-center bg-gray-200 space-x-2 rounded-lg shadow-sm p-2 text-center hover:outline outline-purple-800 text-slate-600 hover:text-purple-800 fill-slate-600 hover:fill-purple-800 hover:bg-gray-300 hover:shadow-md transition-all duration-200"
-                      >
-                        <Github />
-                        <p className="text-sm">View Code</p>
-                      </a>
-                    )}
-                    {project.link && (
-                      <a
-                        href={project.link}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="flex items-center justify-center bg-gray-200 space-x-2 rounded-lg shadow-sm p-2 text-center hover:outline outline-purple-800 text-slate-600 hover:text-purple-800 fill-slate-600 hover:fill-purple-800 hover:bg-gray-300 hover:shadow-md transition-all duration-200"
-                      >
-                        <p className="text-sm">Visit Site</p>
-                        <LinkIcon />
-                      </a>
-                    )}
                   </div>
                 </div>
               </div>
